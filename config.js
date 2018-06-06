4a2db618d8aebb94e22187e947ea215ee66211d6
mhausmann <github@u6.co.za>
2018-06-06 16:32:43 +0100

    updated config

--- config.js
+++ config.js
@@ -7,9 +7,9 @@ window.Retro = {
     "useRevManifest": true,
     "api_base_url": "https://postfacto-api.cfapps.lab01.pcf.pw",
     "websocket_url": "wss://postfacto-api.cfapps.lab01.pcf.pw:443/cable",
-    "contact": "",
-    "terms": "",
-    "privacy": "",
+    "contact": "Matt Hausmann",
+    "terms": "None",
+    "privacy": "None :)",
     "google_oauth_client_id": "601894401748-4n9obnbt31u4ivvc0ei31i6k8jalrtgr.apps.googleusercontent.com"
   }
 }
