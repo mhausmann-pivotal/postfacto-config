07db2940b91f1bb151bbb1565fe963e9d4b98a9a
mhausmann <github@u6.co.za>
2018-06-06 16:54:37 +0100

    pudated config

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 window.Retro = {
   config: {
     "globalNamespace": "Retro",
-    "title": "Postfacto",
+    "title": "UKI PA Postfacto",
     "scripts": ["application.js"],
     "stylesheets": ["application.css"],
     "useRevManifest": true,
     "api_base_url": "https://postfacto-api.cfapps.lab01.pcf.pw",
     "websocket_url": "wss://postfacto-api.cfapps.lab01.pcf.pw:443/cable",
-    "contact": "Matt Hausmann",
-    "terms": "None",
-    "privacy": "None :)",
+    "contact": "https://pivotal.io/contact",
+    "terms": "https://pivotal.io/legal",
+    "privacy": "https://pivotal.io/privacy-policy",
     "google_oauth_client_id": "601894401748-4n9obnbt31u4ivvc0ei31i6k8jalrtgr.apps.googleusercontent.com"
   }
 }
